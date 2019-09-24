a64761c6a2a9f22f2764e4a64fad160fea462a5e
danni <danni.allen@gmail.com>
2019-09-25 09:49:31 +1000

    initial commit

---
+++ Program.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using System.Diagnostics;
+using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace discordvoicebot
+{
+    class Program
+    {
+        private DiscordSocketClient _client;
+
+        // Keep the CommandService and DI container around for use with commands.
+        // These two types require you install the Discord.Net.Commands package.
+        private CommandService _commands;
+        private IServiceProvider _services;
+
+        static void Main(string[] args)
+        => new Program().MainAsync().GetAwaiter().GetResult();
+
+        private Program()
+        {
+            _client = new DiscordSocketClient(new DiscordSocketConfig
+            {
+                LogLevel = LogSeverity.Info,
+                
+                // If you or another service needs to do anything with messages
+                // (eg. checking Reactions, checking the content of edited/deleted messages),
+                // you must set the MessageCacheSize. You may adjust the number as needed.
+                //MessageCacheSize = 50,
+            });
+
+            _commands = new CommandService(new CommandServiceConfig
+            {
+                // Again, log level:
+                LogLevel = LogSeverity.Info,
+                
+                // There's a few more properties you can set,
+                // for example, case-insensitive commands.
+                CaseSensitiveCommands = false,
+            });
+            
+            // Subscribe the logging handler to both the client and the CommandService.
+            _client.Log += Log;
+            _commands.Log += Log;
+            
+            // Setup your DI container.
+            _services = ConfigureServices();
+        }
+
+        public async Task MainAsync()
+        {
+            // Centralize the logic for commands into a separate method.
+            await InitCommands();
+
+            // Remember to keep token private or to read it from an 
+            // external source! In this case, we are reading the token 
+            // from an environment variable. If you do not know how to set-up
+            // environment variables, you may find more information on the 
+            // Internet or by using other methods such as reading from 
+            // a configuration.
+            // TODO fix this
+            /*await _client.LoginAsync(TokenType.Bot, 
+                Environment.GetEnvironmentVariable("DiscordToken"));*/
+            await _client.LoginAsync(TokenType.Bot, 
+                "faketoken");
+            await _client.StartAsync();
+
+            
+            //_client.MessageReceived += MessageReceived;
+
+            // Block this task until the program is closed.
+            await Task.Delay(-1);
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+
+        private async Task InitCommands()
+        {
+            // Either search the program and add all Module classes that can be found.
+            // Module classes MUST be marked 'public' or they will be ignored.
+            // You also need to pass your 'IServiceProvider' instance now,
+            // so make sure that's done before you get here.
+            /*await _commands.AddModulesAsync(Assembly.GetEntryAssembly(), _services);
+            // Or add Modules manually if you prefer to be a little more explicit:
+            await _commands.AddModuleAsync<CommandsModule>(_services);*/
+            // Note that the first one is 'Modules' (plural) and the second is 'Module' (singular).
+
+            // Subscribe a handler to see if a message invokes a command.
+            _client.MessageReceived += HandleCommandAsync;
+        }
+
+        private async Task HandleCommandAsync(SocketMessage arg)
+        {
+            // Bail out if it's a System Message.
+            var msg = arg as SocketUserMessage;
+            if (msg == null) return;
+
+            // We don't want the bot to respond to itself or other bots.
+            if (msg.Author.Id == _client.CurrentUser.Id || msg.Author.IsBot) return;
+            
+            // Create a number to track where the prefix ends and the command begins
+            int pos = 0;
+            if (msg.HasCharPrefix('!', ref pos) /* || msg.HasMentionPrefix(_client.CurrentUser, ref pos) */)
+            {
+                // Create a Command Context.
+                var context = new SocketCommandContext(_client, msg);
+
+                var result = await _commands.ExecuteAsync(context, pos, _services);
+
+                // Uncomment the following lines if you want the bot
+                // to send a message if it failed.
+                // This does not catch errors from commands with 'RunMode.Async',
+                // subscribe a handler for '_commands.CommandExecuted' to see those.
+                //if (!result.IsSuccess && result.Error != CommandError.UnknownCommand)
+                //    await msg.Channel.SendMessageAsync(result.ErrorReason);
+            }
+        }
+
+
+/* 
+        private async Task MessageReceived(SocketMessage message)
+        {
+            switch(message.Content.ToLower())
+            {
+                case "!getvoiceusers":
+                    SocketTextChannel chan = (SocketTextChannel)message.Channel;
+                    foreach (SocketVoiceChannel svc in chan.Guild.VoiceChannels)
+                    {
+                        await message.Channel.SendMessageAsync(svc.Name);
+                        foreach(SocketGuildUser sgu in svc.Users)
+                        {
+                            string name = sgu.Nickname != null ? sgu.Nickname : sgu.Username;
+                            await message.Channel.SendMessageAsync(name);
+                        }
+                    }
+                    break;
+                case "!getservers":
+                    foreach (SocketGuild sg in _client.Guilds)
+                    {
+                        await message.Channel.SendMessageAsync(sg.Name);
+                    }
+                    break;
+                case "!recordvoice":
+                    
+                    break;
+                
+            }
+        }
+
+        private Process CreateStream(string path)
+        {
+            return Process.Start(new ProcessStartInfo
+            {
+                FileName = "ffmpeg",
+                Arguments = $"-hide_banner -loglevel panic -i \"{path}\" -ac 2 -f s16le -ar 48000 pipe:1",
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+            });
+        }
+
+        // The command's Run Mode MUST be set to RunMode.Async, otherwise, being connected to a voice channel will block the gateway thread.
+        [Command("join", RunMode = RunMode.Async)] 
+        public async Task JoinChannel(IVoiceChannel channel = null)
+        {
+            // Get the audio channel
+            channel = channel ?? (Context.User as IGuildUser)?.VoiceChannel;
+            if (channel == null) { await Context.Channel.SendMessageAsync("User must be in a voice channel, or a voice channel must be passed as an argument."); return; }
+
+            // For the next step with transmitting audio, you would want to pass this Audio Client in to a service.
+            var audioClient = await channel.ConnectAsync();
+        }
+
+        private async Task SendAsync(IAudioClient client, string path)
+        {
+            // Create FFmpeg using the previous example
+            using (var ffmpeg = CreateStream(path))
+            using (var output = ffmpeg.StandardOutput.BaseStream)
+            using (var discord = client.CreatePCMStream(AudioApplication.Mixed))
+            {
+                try { await output.CopyToAsync(discord); }
+                finally { await discord.FlushAsync(); }
+            }
+        }
+
+        */
+    }
+}
